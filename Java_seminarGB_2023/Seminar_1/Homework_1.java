ca167ab011810b14b2178f26b0041d130530c6e7
Dmitry <badl1970@mail.ru>
2023-02-10 22:11:50 +0300

    Ввод размера n-го треугольного числа

---
+++ Java_seminarGB_2023/Seminar_1/Homework_1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+// /Написать программу вычисления n-ого треугольного числа.
+
+public class Homework_1 {
+    public static void main(String[] args) {
+        
+    }
+    
+// Ввод  n размера треугольного числа
+
+public static int inputNumber () {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Введите число: ");
+    int num = sc.nextInt();
+    return num;
+}
+
+}
