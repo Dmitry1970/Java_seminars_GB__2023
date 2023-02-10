a4d50e2ff71d89e9e6269517cc91a8c2df6be88e
Dmitry <badl1970@mail.ru>
2023-02-10 22:30:57 +0300

    вычисление и печать треугольного числа

--- Java_seminarGB_2023/Seminar_1/Homework_1.java
+++ Java_seminarGB_2023/Seminar_1/Homework_1.java
@@ -4,16 +4,29 @@ import java.util.Scanner;
 
 public class Homework_1 {
     public static void main(String[] args) {
-        
+    int myDigit = inputNumber();
+    System.out.println("Треугольное число: " + printResult(triangleDigit(myDigit)));
     }
     
 // Ввод  n размера треугольного числа
 
 public static int inputNumber () {
     Scanner sc = new Scanner(System.in);
-    System.out.print("Введите число: ");
+    System.out.print("Введите размер треугольного числа: ");
     int num = sc.nextInt();
     return num;
+}
+
+// Вычисление n-ого треугольного числа
+
+public static int triangleDigit(int number) {
+    int triDigit = (number*(number + 1))/2;
+    return triDigit;
+}
+
+// Печать результата
+public static int printResult(int digit) {
+    return digit;
 }
 
 }
